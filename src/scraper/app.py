348c9b28f3af7aa0d1967496bd54b6bcd85cfa94
Jacob O'Neill <jacoboneill2000@outlook.com>
2024-11-07 20:21:49 +0000

    Update logging config for better looking logs

--- src/scraper/app.py
+++ src/scraper/app.py
@@ -4,6 +4,7 @@ import csv
 import time
 from datetime import datetime, timedelta
 
+
 def get(ticker):
     url = f"https://api.nasdaq.com/api/quote/{ticker}/historical?assetclass=stocks&fromdate=2014-11-07&limit=9999&todate=2024-11-07"
     headers = {
@@ -12,11 +13,14 @@ def get(ticker):
     res = requests.get(url, headers=headers)
     time.sleep(1)
     if res.json()["status"]["rCode"] != 200:
-        logging.error(f"Request for {ticker} failed with status code {res.json()['status']['rCode']}.")
+        logging.error(
+            f"Request for {ticker} failed with status code {res.json()['status']['rCode']}."
+        )
         return None
     else:
         logging.info(f"Request for {ticker} complete.")
         return res.json()["data"]["tradesTable"]["rows"]
+
 
 def parse_data(data, ticker):
     result = {
@@ -25,11 +29,15 @@ def parse_data(data, ticker):
         "price": (float(data["open"][1:]) + float(data["close"][1:])) / 2,
     }
     return result
-    
+
 
 if __name__ == "__main__":
     # Logging config
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - [%(levelname)s] %(message)s', datefmt='%d-%m-%Y %H:%M:%S')
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - [%(levelname)s] %(message)s",
+        datefmt="%d-%m-%Y %H:%M:%S",
+    )
 
     # Scrape data from NASDAQ and parse
     data = []
